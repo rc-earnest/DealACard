7c07fbaeafc6e6c72b88fe118a5da56df9088230
rc-earnest <rc.earnest04@gmail.com>
2025-10-23 10:53:29 -0600

    added deck class, card class and code to run both of those things

--- DealACard/DealACard.vb
+++ DealACard/DealACard.vb
@@ -1,3 +1,22 @@
 ﻿Public Class DealACard
+    Dim theDeck As New Deck() 'put this in GameLogic class later
+    Private Sub MainForm_Click(sender As Object, e As EventArgs) Handles Me.Click
+        'Dim ah As New Card("a", "h")
+        ' Dim twoS As New Card("q", "h")
+        'Dim cardName As String = "_AS"
+        'Dim cardImage As Image = My.Resources.ResourceManager.GetObject(cardName) ' My.Resources._AS
+        'Dim currentCard As New Card("","")
+        Dim g As Graphics = Me.CreateGraphics
+
+
+
+        'g.DrawImage(ah.backImage, 10, 10)
+        g.DrawImage(theDeck.DealCard.frontImage, 100, 100)
+
+        'g.DrawImage(twoS.frontImage, 150, 150)
+
+
+        g.Dispose()
+    End Sub
 
 End Class
---
+++ DealACard/Deck.vb
@@ -0,0 +1,69 @@
+﻿Public Class Deck
+    Private _deck As New Stack(Of Card)
+
+
+    Sub test()
+        Dim ah As New Card("a", "h")
+        Dim asp As New Card("a", "s")
+        Dim ad As New Card("a", "d")
+        Dim ac As New Card("a", "c")
+        Me._deck.Push(ah)
+        Me._deck.Push(asp)
+        Me._deck.Push(ad)
+        Me._deck.Push(ac)
+    End Sub
+    ''' <summary>
+    ''' Deals one card off the top of the deck
+    ''' </summary>
+    ''' <returns></returns>
+    Function DealCard() As Card
+        Return _deck.Pop()
+    End Function
+    ''' <summary>
+    ''' returns the number of cards remaining in the deck
+    ''' </summary>
+    ''' <returns></returns>
+    Function CardsRemaining() As Integer
+        Return _deck.Count
+    End Function
+    ''' <summary>
+    ''' "shuffles" the deck by creating a new ordered deck
+    ''' </summary>
+    Sub shuffle()
+        Dim _suit$, _rank$
+        For suit = 1 To 4
+
+            Select Case suit
+                Case 1
+                    _suit = "s"
+                Case 2
+                    _suit = "d"
+                Case 3
+                    _suit = "c"
+                Case 4
+                    _suit = "h"
+            End Select
+            For rank = 1 To 13
+                Select Case rank
+                    Case 1
+                        _rank = "a"
+                    Case 2 To 10
+                        _rank = CStr(rank)
+                    Case 11
+                        _rank = "j"
+                    Case 12
+                        _rank = "q"
+                    Case 13
+                        _rank = "k"
+                End Select
+                Dim newCard As New Card(_rank, _suit)
+                Me._deck.Push(newCard)
+            Next
+        Next
+    End Sub
+
+    Sub New()
+        shuffle()
+    End Sub
+
+End Class
